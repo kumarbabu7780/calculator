1dc53d5c242d26299c3662e1d27a3807a6411dae
kumarbabu7780 <kumarbabutatapudi10@gmail.com>
2022-12-01 15:42:11 +0530

    first project structure

---
+++ src/CalculatorManger.java
@@ -0,0 +1,34 @@
+class Calculator
+{
+   int first;
+   int second;
+
+   int sumResult;
+   int subResult;
+   int mulResult;
+   int remResult;
+   double divResult;
+    
+   Calculator(int a,int b)
+   {
+      first=a;
+      second=b;
+    }
+
+   public viod performAdd()
+   {
+     sumResult=first+secound;
+   }
+   
+}
+
+class CalculatorManager
+{
+    public satatic void main(String[]args)
+    {
+       Calculator cl=new Calculator(10,5);
+       c1.performAdd();
+       c1.dispalySum();
+
+     }
+}
